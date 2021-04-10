36cca7fee9d506d5e3de5d5f1eb55b64f29c27b0
Phoebe Pan <phoebe.pan@ytml.com.au>
2021-04-10 18:38:53 +1000

    restruct app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,20 +6,27 @@ import { Page1Component } from './page1/page1.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ColorComponent } from './utilities/color/color.component';
 import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
-  { path: 'page1', component: Page1Component },
-  { path: 'page2', component: Page2Component, canActivate: [AuthGuard] },
-  { path: 'dashboard', component: DashboardComponent },
-  {
-    path: 'utilities',
-    children: [
-      { path: 'color', component: ColorComponent },
-      { path: 'color/:type', component: ColorComponent }
+  { path: '', component: LayoutComponent, children:
+    [
+      { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+      { path: 'page1', component: Page1Component },
+      { path: 'page2', component: Page2Component, canActivate: [AuthGuard] },
+      { path: 'dashboard', component: DashboardComponent },
+      {
+        path: 'utilities',
+        children: [
+          { path: 'color', component: ColorComponent },
+          { path: 'color/:type', component: ColorComponent }
+        ]
+      },
+      { path: 'components', loadChildren: () => import('./components/components.module').then(m => m.ComponentsModule) },
     ]
   },
-  { path: 'components', loadChildren: () => import('./components/components.module').then(m => m.ComponentsModule) },
+  { path: 'login', component: LoginComponent },
   { path: '**', component: NotFoundComponent }
 ];
 
