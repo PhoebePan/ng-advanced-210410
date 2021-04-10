6c2f80d912025584787d74f9c24b57a601d74518
Phoebe Pan <phoebe.pan@ytml.com.au>
2021-04-10 14:06:21 +1000

    add activateRoute

--- src/app/utilities/color/color.component.ts
+++ src/app/utilities/color/color.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
 
 @Component({
   templateUrl: './color.component.html',
   styleUrls: ['./color.component.css']
 })
 export class ColorComponent implements OnInit {
+  type = 0;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.type = +this.route.snapshot.paramMap.get('type');
+
+    this.route.paramMap.subscribe(params => {
+      this.type = +params.get('type');
+    });
+  }
+
+  plusOne() {
+    this.router.navigate(['/utilities/color/', this.type+1]);
   }
 
 }
